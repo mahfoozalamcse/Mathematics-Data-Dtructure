0c8611ef9f1c23c50921cd906a4a9d0476ea99b3
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-28 08:44:44 +0000

    solve count problem

---
+++ MathPractice.java
@@ -0,0 +1,28 @@
+public class MathPractice {
+
+    // count digit
+    public static int countDigit(int x){
+        int count  = 0;
+        while (x > 0) {
+           x  =  x / 10;
+           count++;
+        }
+        return count;
+    }
+    // pallindrome number check
+    public static boolean pallindrome(int x){
+        int temp = x;
+        int rev = 0;
+        while(temp != 0){
+            rev = rev*10 + temp % 10;
+            temp = temp / 10;
+        }
+        
+        return (rev == x);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countDigit(100));
+        System.out.println(pallindrome(78987));
+    }
+}
