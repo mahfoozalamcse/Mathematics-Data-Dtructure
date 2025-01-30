6b9e4216db9eef7ef7365fec02062df34227d5d8
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-30 08:30:46 +0000

    find gcd 3 way solution

--- MathPart2.java
+++ MathPart2.java
@@ -9,9 +9,30 @@ public class MathPart2 {
     }
 
     // efficient solution
-     
+    public static int gcd2(int a, int b){
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // gcd 3 way solution
+    public static int gcd3(int a, int b){
+        int res = Math.min(a, b);
+        while (res > 0) {
+            if (a % res == 0 && b % res == 0) {
+                break;
+            }
+            res--;
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         System.out.println(gcd(100, 200));
+        System.out.println(gcd2(100, 200));
+        System.out.println(gcd3(100, 200));
     }
 }
