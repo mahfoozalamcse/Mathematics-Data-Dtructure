21c999a3d805a138f76cc44be4ed6a09750c8f48
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-21 11:56:00 +0530

    Update MathPractice.java

--- MathPractice.java
+++ MathPractice.java
@@ -4,7 +4,7 @@ public class MathPractice {
     public static int countDigit(int x){
         int count  = 0;  
         while (x > 0) {
-           x  =  x / 10;
+           x  =  x / 10; 
            count++;
         }
         return count;
