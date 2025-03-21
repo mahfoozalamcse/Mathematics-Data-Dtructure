6f10d21c4f35e00e78d263268aef30135d64267d
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-21 11:58:34 +0530

    Update MathPart2.java

--- MathPart2.java
+++ MathPart2.java
@@ -20,7 +20,7 @@ public class MathPart2 {
 
     // gcd 3 way solution
     public static int gcd3(int a, int b){
-        int res = Math.min(a, b);
+        int res = Math.min(a, b);  
         while (res > 0) {
             if (a % res == 0 && b % res == 0) {
                 break;
