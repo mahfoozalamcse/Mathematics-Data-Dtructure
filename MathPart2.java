76d1c4e3f933344833e8846745166ab6dea04150
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-27 16:01:05 +0530

    Update MathPart2.java

--- MathPart2.java
+++ MathPart2.java
@@ -2,7 +2,7 @@ public class MathPart2 {
   
     // Neive method gcd 
     public static int gcd(int a, int b){
-        if ( b == 0) { 
+        if ( b == 0) {  
             return a;
         }
         return gcd(b, a % b);
